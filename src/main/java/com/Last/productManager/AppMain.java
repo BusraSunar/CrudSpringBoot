c2bb13caa22b2523eaef7670a20ab07d425f50cb
Busra <54499947+Busra@users.noreply.github.com>
2020-02-19 16:20:47 +0300

    Third Commit

--- src/main/java/com/Last/productManager/AppMain.java
+++ src/main/java/com/Last/productManager/AppMain.java
@@ -3,6 +3,8 @@ package com.Last.productManager;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
  
@@ -10,9 +12,13 @@ import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 @ComponentScan(basePackages={"com.Last.Controller", "com.Last.service","com.Last.Repository"})
 @EntityScan(basePackages = {"com.Last.Model"} )
 @EnableJpaRepositories(basePackages = {"com.Last.Repository"})
-public class AppMain {
+public class AppMain extends SpringBootServletInitializer{
 	
     public static void main(String[] args) {
         SpringApplication.run(AppMain.class, args);
+    }
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+        return application.sources(AppMain.class);
     }
 }
