91dacd46a2297dda73659e9c34703b6549b2ab04
Busra <54499947+Busra@users.noreply.github.com>
2020-02-19 12:19:17 +0300

    Second Commit

--- src/main/java/com/Last/productManager/AppMain.java
+++ src/main/java/com/Last/productManager/AppMain.java
@@ -3,15 +3,8 @@ package com.Last.productManager;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
-import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
-import org.springframework.orm.jpa.LocalEntityManagerFactoryBean;
-import org.springframework.web.servlet.view.InternalResourceViewResolver;
-import org.springframework.web.servlet.view.JstlView;
-import org.thymeleaf.spring5.templateresolver.SpringResourceTemplateResolver;
-import org.thymeleaf.templatemode.TemplateMode;
-import org.thymeleaf.templateresolver.ITemplateResolver;
  
 @SpringBootApplication
 @ComponentScan(basePackages={"com.Last.Controller", "com.Last.service","com.Last.Repository"})
